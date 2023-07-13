17d800f15c2a55dea4e9d17a4f427c35433bbd58
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-13 12:20:37 +0200

    Meals component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,17 @@
 import React, {Fragment}from 'react';
 
-import mealsImage from '../../assets/meals.jpg'
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
 
 export default function Header(props) {
   return (
     <Fragment>
-        <header>
+        <header className={classes.header}>
         <h1>Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
         </header>
-        <div>
+        <div className={classes['main-image']}>
             <img src={mealsImage}/>
         </div>
     </Fragment>
