6402608f2ec2577cf9e7dc4e8cadc5ab2dc07602
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-14 11:38:56 +0200

    updating cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === "ADD")
+    return defaultCartState;
+};
+
 export default function CartProvider(props) {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
     const addItemToCartHandler = (item) => {
-
+        dispatchCartAction({type: 'ADD', item: item})
     };
 
     const removeItemFromCartHandler = (id) => {
-
+        dispatchCartAction({ type: 'REMOVE' , id: id})
     };
 
     const cartContext ={
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler ,
         removeItem:removeItemFromCartHandler,
     }
