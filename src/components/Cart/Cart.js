49077becd2b042712e3ab737b29e9c319371c988
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-17 12:35:44 +0200

    adding css

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 export default function Cart(props) {
   const cartCtx = useContext(CartContext);
@@ -9,11 +10,26 @@ export default function Cart(props) {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {
+
+  };
+
+  const cartItemAddHandler = (item) => {
+
+  };
+
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li key={item.id}>{item.name}</li>
+        <CartItem  
+        key={item.id} 
+        name={item.name} 
+        amount={item.amount} 
+        price={item.price}
+        onRemove ={cartItemRemoveHandler.bind(null, item.id)}
+        onAdd = {cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
